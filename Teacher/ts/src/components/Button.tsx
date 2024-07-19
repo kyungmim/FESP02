82356e68c84893f1c6bdab9e3b5672f09a29e65c
kyungmim <kyungmin727@gmail.com>
2024-07-19 18:09:29 +0900

    버튼 컴포넌트 ts마이그레이션 완료

--- Teacher/ts/src/components/Button.tsx
+++ Teacher/ts/src/components/Button.tsx
@@ -1,22 +1,38 @@
-export default function Button({ children, type = 'button', bgColor = 'orange', size = 'md', ...rest }) {
-  let btnColor = {
-    gray: `bg-gray-900`,
-    orange: 'bg-orange-500',
-    red: 'bg-red-500',
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  bgColor?: "gray" | "red" | "blue";
+  btnSize?: "sm" | "md" | "lg";
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  type = "button",
+  bgColor = "gray",
+  btnSize = "sm",
+  ...rest
+}) => {
+  const btnColor = {
+    gray: "bg-gray-500 hover:bg-gray-600",
+    blue: "bg-blue-500 hover:bg-blue-600",
+    red: "bg-red-500 hover:bg-red-600",
   };
-  let btnSize = {
-    sm: 'py-1 px-2 text-sm',
-    md: 'py-1 px-4 text-base',
-    lg: 'py-2 px-6 text-lg',
+
+  const size = {
+    sm: "py-1 px-2 text-sm",
+    md: "py-2 px-4 text-base",
+    lg: "py-2 px-6 text-lg",
   };
 
   return (
     <button
-      type={ type }
-      className={`${ btnColor[bgColor] } ${ btnSize[size] } text-white font-semibold ml-2 text-base hover:bg-amber-400 rounded`}
-      { ...rest }
+      className={`${btnColor[bgColor]} ${size[btnSize]} ml-2 text-white rounded`}
+      type={type}
+      {...rest}
     >
-      { children }
+      {children}
     </button>
   );
-}
+};
+
+export default Button;
